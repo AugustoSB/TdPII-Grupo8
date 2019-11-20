6ba31c526d7902762f4a17bd060cc837aa824953
AugustoSB <auchusto1996@yahoo.com.ar>
2019-11-20 19:44:09 -0300

    html con mejoras y usa bootstrap - re hermo

--- servidor/static/scripts.js
+++ servidor/static/scripts.js
@@ -1,12 +1,30 @@
 function back_call(llamado){
-	var node = document.createElement("LI");                 // Create a <li> node
-	cadena = llamado.replace("_", " ");
-	var d = new Date();
+
+if(llamado == 'ultrasonido_on'){
+	document.getElementById("ul_on").style.display = 'none';
+	document.getElementById("ul_off").style.display = 'inline-block';
+}else{
+	document.getElementById("ul_on").style.display = 'inline-block';
+	document.getElementById("ul_off").style.display = 'none';
+}
+
+var d = new Date();
 	var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-	cadena = cadena + " ___ " + time;
-	var textnode = document.createTextNode(cadena);         // Create a text node
-	node.appendChild(textnode);                              // Append the text to <li>
-	document.getElementById("lista_historial").appendChild(node);     // Append <li> to <ul> with id="myList" 
+
+var table = document.getElementById("lista_historial");
+
+// Create an empty <tr> element and add it to the 1st position of the table:
+var row = table.insertRow(0);
+
+// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+
+// Add some text to the new cells:
+cell1.innerHTML = llamado;
+cell2.innerHTML = time; 
+
+
 	$.ajax({
 		url: '/'.concat(llamado)
 	});
